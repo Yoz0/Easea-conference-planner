7eb4490fb311e1bfc1285bd085fc2d987659af97
Clément Saintier <clement.saintier@gmail.com>
2017-05-27 12:42:08 +0200

    Add scripts to build and clean

---
+++ buildAndRun.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+easea csdc17_Kastenbaum_Saintier.ez && make && ./csdc17_Kastenbaum_Saintier
+
---
+++ clean.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+rm csdc17_Kastenbaum_Saintier.cpp           2> /dev/null
+rm csdc17_Kastenbaum_Saintier.csv           2> /dev/null
+rm csdc17_Kastenbaum_Saintier.mak           2> /dev/null
+rm csdc17_Kastenbaum_Saintier.o             2> /dev/null
+rm csdc17_Kastenbaum_Saintier.prm           2> /dev/null
+rm csdc17_Kastenbaum_Saintier.dat           2> /dev/null
+rm csdc17_Kastenbaum_SaintierIndividual.cpp 2> /dev/null
+rm csdc17_Kastenbaum_SaintierIndividual.hpp 2> /dev/null
+rm csdc17_Kastenbaum_SaintierIndividual.o   2> /dev/null
+rm Makefile                                 2> /dev/null
+
